f33ac0c0231e50d99cebf4d9356d923999774fce
HOME <HOME@DESKTOP-993FB2V>
2026-02-06 13:19:12 +0530

    First Commit

---
+++ GrocceryApplicationProject/src/main/java/utilities/ExcelUtility.java
@@ -0,0 +1,23 @@
+package utilities;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelUtility {
+	static FileInputStream f;
+	static XSSFWorkbook w;
+	static XSSFSheet s;
+	public static String getStringData(int a, int b, String sheet) throws IOException {
+		f=new FileInputStream("C:\\Users\\HOME\\eclipse-workspace\\GrocceryApplicationProject\\src\\test\\resources\\TestData.xlsx");
+		w=new XSSFWorkbook(f);
+		s=w.getSheet(sheet);
+		XSSFRow row=s.getRow(a);
+		XSSFCell cell=row.getCell(b);
+		return cell.getStringCellValue();
+	}
+}
---
+++ GrocceryApplicationProject/src/test/java/testScripts/LoginTest.java
@@ -0,0 +1,51 @@
+package testScripts;
+
+import java.io.IOException;
+
+import org.testng.annotations.Test;
+
+import automationCore.Base;
+import pages.LoginPage;
+import utilities.ExcelUtility;
+
+public class LoginTest extends Base {
+	@Test
+	public void verifyUserWithValidCredentials() throws IOException {
+		String usernameValue=ExcelUtility.getStringData(0, 0, "LoginPage");
+		String passwordValue=ExcelUtility.getStringData(0, 1, "LoginPage");
+		LoginPage page=new LoginPage(driver);
+		page.enterUsernameOnUsernameField(usernameValue);
+		page.enterPasswordOnPasswordField(passwordValue);
+		page.clickOnSignInButton();
+	}
+	
+	@Test
+	public void verifyUserWithValidUsernameAndInvalidPassword() throws IOException {
+		String usernameValue=ExcelUtility.getStringData(1, 0, "LoginPage");
+		String passwordValue=ExcelUtility.getStringData(1, 1, "LoginPage");
+		LoginPage page=new LoginPage(driver);
+		page.enterUsernameOnUsernameField(usernameValue);
+		page.enterPasswordOnPasswordField(passwordValue);
+		page.clickOnSignInButton();
+	}
+	
+	@Test
+	public void verifyUserWithInvalidUsernameAndValidPassword() throws IOException {
+		String usernameValue=ExcelUtility.getStringData(2, 0, "LoginPage");
+		String passwordValue=ExcelUtility.getStringData(2, 1, "LoginPage");
+		LoginPage page=new LoginPage(driver);
+		page.enterUsernameOnUsernameField(usernameValue);
+		page.enterPasswordOnPasswordField(passwordValue);
+		page.clickOnSignInButton();
+	}
+	
+	@Test
+	public void verifyUserWithInvalidCredentials() throws IOException {
+		String usernameValue=ExcelUtility.getStringData(3, 0, "LoginPage");
+		String passwordValue=ExcelUtility.getStringData(3, 1, "LoginPage");
+		LoginPage page=new LoginPage(driver);
+		page.enterUsernameOnUsernameField(usernameValue);
+		page.enterPasswordOnPasswordField(passwordValue);
+		page.clickOnSignInButton();
+	}
+}
